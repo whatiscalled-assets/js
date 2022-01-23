e0f6bdf8278ea5913e3566506d6e7a90889b57ac
whatiscalled-assets <whatiscalled@protonmail.com>
2022-01-23 13:13:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("whatiscalled-assets");
+var cmt_list = document.querySelector("#box_outer_view").children
+
+for (var i=0; i < cmt_list.length; i++) {
+    var body = cmt_list[i].querySelector("h3.answer-play").innerText;
+    var body_no_space = body.replaceAll(/\s/g, '') ;
+    if (body_no_space.startsWith("Whatisitcalled") || body_no_space.startsWith("Correct")){
+        cmt_list[i].style.display = "none";
+    }
+}
